607e8cb61c92d685b7cb4ab3e8f3844798a31076
Ashish Gupta <122588081+ashishgupta-git@users.noreply.github.com>
2024-01-30 16:01:52 +0530

    Add files via upload

---
+++ if else calculator.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+operator = input("What to perform: ")
+
+if operator== "+":
+    print(num1+num2)
+elif operator == "-":
+    print(num1-num2)
+elif operator == "*":
+    print(num1*num2)
+elif operator == "/":
+    print(num1/num2)
+elif operator == "%":
+    print(num1%num2)
+elif operator == "//":
+    print(num1//num2)
+else:
+    print("Invalid Input")
